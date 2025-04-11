60d6df5588444d8b73081558769200053ba3c74b
Lev Romanov <levromanov@MacBook-Pro-Lev.local>
2025-04-11 11:35:05 +0300

    changed apiUrl

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const apiUrl = "http://localhost:8000/api/v1";
+export const apiUrl = "http://194.87.95.139/api/v1";
 export const S3_URL = "https://8550b56c-0f5c-48cb-9e3f-ff7817b0cfe3.selstorage.ru/";
 export const S3_ITEM_TYPE = ".webp";
 
